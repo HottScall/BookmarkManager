1fae82afe3571bc7b24436fcfe5653a2dfe28973
Scott <Scotthall1983@hotmail.com>
2019-09-28 20:09:32 +0100

    minor changes made to viewing_bookmark_spec in order to pass failing tests. Primart key is already set up, so stating the id menually is no longer needed

--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -2,9 +2,9 @@ feature 'Viewing bookmarks' do
   scenario 'A user can see a list of bookmarks' do
     connection = PG.connect(dbname: 'bookmark_manager_test')
 
-    connection.exec("INSERT INTO bookmarks VALUES (1, 'http://www.skysports.com');")
-    connection.exec("INSERT INTO bookmarks VALUES (2, 'https://www.linkedin.com/');")
-    connection.exec("INSERT INTO bookmarks VALUES (3, 'https://news.sky.com/');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.skysports.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES ('https://www.linkedin.com/');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES ('https://news.sky.com/');")
 
     visit ('/bookmarks')
 
