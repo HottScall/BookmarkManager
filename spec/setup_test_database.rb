7412816f65d7cd6d840f36649489383277a8fd8f
Scott <Scotthall1983@hotmail.com>
2019-09-29 13:50:22 +0100

    Made a change to the setup_test_datase file to fix an issue where the error message couldn't find the setup database method. Tidied up the spec helper a little

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -1,7 +1,9 @@
 require 'pg'
 
-p "Setting up test database...."
+def setup_test_database
 
 connection = PG.connect(dbname: 'bookmark_ruby_test')
 
 connection.exec("TRUNCATE bookmarks;")
+
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,6 @@
-require_relative './setup_test_database'
-
+# Set the environment to "test"
 ENV['ENVIRONMENT'] = 'test'
 
-RSpec.configure do |config|
-  config.before(:each) do
-    setup_test_database
-  end
-end
-# Set the environment to "test"
-ENV['RACK_ENV'] = 'test'
 
 # Bring in the contents of the `app.rb` file
 require File.join(File.dirname(__FILE__), '..', 'app.rb')
@@ -17,6 +9,7 @@ require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
+require_relative './setup_test_database'
 
 # Tell Capybara to talk to BookmarkManager
 Capybara.app = BookmarkManager
@@ -39,6 +32,11 @@ Capybara.app = BookmarkManager
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+
+  config.before(:each) do
+    setup_test_database
+  end
+
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
