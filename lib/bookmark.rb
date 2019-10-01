04cf97556acb7994add71af89cc155cbb1012d66
Scott <Scotthall1983@hotmail.com>
2019-10-01 15:39:37 +0100

    Added a .delete test to bookmark_spec file. Added the self.delete method to bookmark.rb. Rewrote the controller to remove the PG.connect command and replaced it with Bookmark.delete command.

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -25,6 +25,16 @@ class Bookmark
     Bookmark.new(id: result[0]['id'], title: result[0]['title'], url: result[0]['url'])
   end
 
+  def self.delete(id:)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'bookmark_ruby_test')
+    else
+      connection = PG.connect(dbname: 'bookmark_ruby')
+    end
+
+    connection.exec("DELETE FROM bookmarks WHERE id = #{id}")
+  end
+
   attr_reader :id, :title, :url
 
   def initialize(id:, title:, url:)
